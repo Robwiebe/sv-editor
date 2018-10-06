9d966a4b7f859adc537947daf99ddcf99c8af239
Rob Wiebe <rjw.ywam@gmail.com>
2018-10-06 06:55:45 -1000

    Adding more languages & sorting by ABC

--- src/components/LanguageInput.js
+++ src/components/LanguageInput.js
@@ -8,20 +8,24 @@ const LanguageInput = (props) => {
             <p style={{textDecoration: 'underline', color: 'darkred'}}>SELECT YOUR LANGUAGE:</p>
             <select selected={props.currentValue} name="Language" onChange={props.languageChange} style={{fontSize: '15px', height: '30px', margin: '10px'}}>
                 <option value='null'>-</option>
-                <option value='English'>English</option>
-                <option value='UniSkript-English'>UniSkript (English)</option>
-                <option value='Thai'>Thai</option>
-                <option value='Spanish'>Spanish</option>
-                <option value='Korean'>Korean</option>
-                <option value='Portuguese'>Portuguese</option>
-                <option value='Russian'>Russian</option>
-                <option value='Uzbek'>Uzbek</option>
-                <option value='Kazakh'>Kazakh</option>
-                <option value='Kyrgyz'>Kyrgyz</option>
-                <option value='Simplified-Chinese'>Simplified Chinese</option>
-                <option value='Hebrew'>Hebrew</option>
-                <option value='Greek'>Greek</option>
-                <option value='French'>French</option>
+                <option value="Afrikaans">Afrikaans</option>
+                <option value="English">English</option>
+                <option value="Finnish">Finnish</option>
+                <option value="French">French</option>
+                <option value="German">German</option>
+                <option value="Greek">Greek</option>
+                <option value="Hebrew">Hebrew</option>
+                <option value="Kazakh">Kazakh</option>
+                <option value="Korean">Korean</option>
+                <option value="Kyrgyz">Kyrgyz</option>
+                <option value="Norwegian">Norwegian</option>
+                <option value="Portuguese">Portuguese</option>
+                <option value="Russian">Russian</option>
+                <option value="Simplified Chinese">Simplified Chinese</option>
+                <option value="Spanish">Spanish</option>
+                <option value="Thai">Thai</option>
+                <option value="UniSkript (English)">UniSkript (English)</option>
+                <option value="Uzbek">Uzbek</option>
             </select>
             <hr />
             
